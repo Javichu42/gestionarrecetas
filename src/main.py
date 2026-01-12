991b47538ca9e4fbd05f91df3baca4a7da452dd0
Javichu <ljavi72467@gmail.com>
2026-01-12 20:30:36 +0100

    Creamos funcion mostrarmenu

--- src/main.py
+++ src/main.py
@@ -1,3 +1,13 @@
+def mostrarmenu():
+    print("\n=== Gestor de Recetas ===")
+    print("1. Añadir Receta")
+    print("2. Listar Receta")
+    print("3. Exportar Receta (TXT, CSV, JSON)")
+    print("4. Importar Receta (TXT, CSV, JSON)")
+    print("5. Buscar Receta online (API)")
+    print("6. Ver Estadisticas")
+    print("0. Salir")
+
 def main():
     print("Gestor de Recetas")
 
